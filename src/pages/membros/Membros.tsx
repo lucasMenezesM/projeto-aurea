047f4c292c12787de7561947ae96cf711074974b
Lucas Menezes <livedolux@gmail.com>
2025-08-06 16:23:06 -0300

    implementa react router dom e configura rotas iniciais

---
+++ src/pages/membros/Membros.tsx
@@ -0,0 +1,9 @@
+const Membros: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-9xl font-bold underline text-blue-800">Membros</h1>
+    </div>
+  );
+};
+
+export default Membros;
---
+++ src/pages/sobre/Sobre.tsx
@@ -0,0 +1,9 @@
+const Sobre: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-9xl font-bold underline text-blue-800">Sobre</h1>
+    </div>
+  );
+};
+
+export default Sobre;
