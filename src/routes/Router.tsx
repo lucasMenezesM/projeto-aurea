750a7af03667a90e4dcac1f4677ddba01e819046
Lucas Menezes <livedolux@gmail.com>
2025-08-15 15:09:06 -0300

    cria footer da aplicação

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,15 +2,19 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "../pages/home/Home";
 import Membros from "../pages/membros/Membros";
 import Sobre from "../pages/sobre/Sobre";
+import { Navbar } from "@/pages/shared/navbar/navbar";
+import Footer from "@/pages/shared/footer/Footer";
 
 export const Router: React.FC = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/membros" element={<Membros />} />
         <Route path="/sobre" element={<Sobre />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
